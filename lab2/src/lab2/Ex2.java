f7d9cba9ce51550c0bcac742b9c9b4048cb14c0f
Uma Poornima <Uma Poornima@192.168.0.106>
2021-04-03 17:28:02 +0530

    ex2

--- lab2/src/lab2/Ex2.java
+++ lab2/src/lab2/Ex2.java
@@ -1,10 +1,38 @@
 package lab2;
 
 public class Ex2 {
+	public static String[] sortString(String[] str) {
+		String[] result = new String[str.length];
+		int mid = 0;
+		for (int i = 0; i < str.length; i++) {
+			for (int j = i + 1; j < str.length - 1; j++) {
+				if (str[i].compareTo(str[j]) > 0) {
+					String ex = str[i];
+					str[i] = str[j];
+					str[j] = ex;
+				}
+			}
+		}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < str.length; i++) {
+			if (str[i].length() % 2 != 0) {
+				mid = (str[i].length() + 1) / 2;
+			} else {
+				mid = (str[i].length() / 2);
+			}
+			result[i] = str[i].substring(0, mid).toUpperCase().concat(str[i].substring(mid, str[i].length()));
 
+		}
+
+		for (String S : result) {
+			System.out.println(S);
+		}
+		return str;
 	}
 
+	public static void main(String[] args) {
+		String[] str = { "pooja","akash","punith","priya","revanth","appu" };
+
+		sortString(str);
+	}
 }
