1dd2c2a43e2304fb0b65b64dc6e85aee9a7f4a55
Uma Poornima <Uma Poornima@192.168.0.106>
2021-04-03 16:25:21 +0530

    ex5

---
+++ lab1/src/lab1/Ex5.java
@@ -0,0 +1,20 @@
+package lab1;
+	
+public class Ex5 {
+
+	public static int calculateSum(int n) {
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				sum += i;
+			}
+		}
+
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(calculateSum(10));
+	}
+
+}
