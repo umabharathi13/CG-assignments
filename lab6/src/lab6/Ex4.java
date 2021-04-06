dac90df89a70d92d0bfe14a9f69e240a3ac318e7
Uma Poornima <Uma Poornima@192.168.0.106>
2021-04-06 11:08:14 +0530

    ex4

---
+++ lab6/src/lab6/Ex4.java
@@ -0,0 +1,36 @@
+package lab6;
+
+import java.util.Scanner;
+
+public class Ex4 {
+	static final int MAX_CHAR = 256;
+
+	static void getOccuringChar(String str) {
+		int count[] = new int[MAX_CHAR];
+
+		int len = str.length();
+		for (int i = 0; i < len; i++)
+			count[str.charAt(i)]++;
+		char ch[] = new char[str.length()];
+		for (int i = 0; i < len; i++) {
+			ch[i] = str.charAt(i);
+			int find = 0;
+			for (int j = 0; j <= i; j++) {
+
+				
+				if (str.charAt(i) == ch[j])
+					find++;
+			}
+
+			if (find == 1)
+				System.out.println("Number of Occurrence of " + str.charAt(i) + " is:" + count[str.charAt(i)]);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String str = sc.next();
+		getOccuringChar(str);
+	}
+}
