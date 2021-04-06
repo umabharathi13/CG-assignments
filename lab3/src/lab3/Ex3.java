88fd6b3d58bdbabc4b8f63bc21e2a1b0e21587e0
Uma Poornima <Uma Poornima@192.168.0.106>
2021-04-06 11:07:23 +0530

    ex3

--- lab3/src/lab3/Ex3.java
+++ lab3/src/lab3/Ex3.java
@@ -1,10 +1,32 @@
 package lab3;
 
+import java.util.Scanner;
 public class Ex3 {
-
+	String alterString(String str)
+	{
+		String str1="";
+		int num=0;
+		for(int i=0;i<str.length();i++)
+		{
+			char c=str.charAt(i);
+			if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+			{
+				str1+=c;
+			}
+			else
+			{
+				num=1+(int)c;
+				str1=str1+(char)num;
+			}
+		}
+		return str1;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		Ex3 e=new Ex3();
+		System.out.println(e.alterString(str));
+		
 	}
 
 }
